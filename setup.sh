7d4f73e626933af26c3ab824910ab7755a598231
Joseph Collard <jcollard@chadwickschool.org>
2020-10-18 10:00:12 -0700

    Updated setup.sh and setup-complete.txt

--- setup.sh
+++ setup.sh
@@ -11,10 +11,10 @@ if [ -f "$ROOT/.setup-date" ]; then
 fi
 
 echo "Running setup!"
-set -x
+set -e
 touch "$ROOT/.setup-date"
 cp "$ROOT/util/pre-commit" "$ROOT/.git/hooks/"
 cp "$ROOT/util/pre-commit" "$ROOT/.git/hooks/"
 git config commit.template "$ROOT/setup/commit-template"
-
+git commit --allow-empty -m "Ran setup."
 cat util/setup-complete.txt
