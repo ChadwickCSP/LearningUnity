480f0b627fd993dbbbbc0c63bf040d264fafd6d1
Joseph Collard <jcollard@chadwickschool.org>
2020-10-18 07:28:56 -0700

    Added setup script
    Created a setup script to initialize the pre-commit and commit-template

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [ -f "$ROOT/.setup-date" ]; then
+    echo "This setup script has already been run. Are you sure you would like to run it again?"
+    read -p "Type YES to continue? " ACTION
+    if [ ! "$ACTION" = "YES" ]; then
+        echo "Aborting setup."
+        exit 0;
+    fi
+fi
+
+echo "Running setup!"
+set -x
+touch "$ROOT/.setup-date"
+cp "$ROOT/util/pre-commit" "$ROOT/.git/hooks/"
+cp "$ROOT/util/pre-commit" "$ROOT/.git/hooks/"
+git config commit.template "$ROOT/setup/commit-template"
